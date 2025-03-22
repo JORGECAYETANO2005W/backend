18a82762238db9c797d1ee715b4f802b0075fa61
Jorge Miguel <jorgecayetano227@gmail.com>
2025-03-22 11:18:50 -0600

    cambio

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { connectDB } from './db.js';
 // Conectar a la base de datos
 connectDB();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
